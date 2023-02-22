527a9bd43118b79a7c19c696e0f2e66b0f0963bb
tanuj <tanujbhatt644@gmail.com>
2023-02-22 05:40:59 +0530

    Uploading Server Side...!

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors  = require("cors")
+const addvechile = require("./routes/AddVechile")
+const search_ride = require("./routes/SearchRides")
+const signin = require("./routes/User")
+const settings = require("./routes/Settings")
+const path = require("path")
+// require("./client/build/");
+
+//connecting env file
+dotenv.config();
+
+
+
+
+
+//connecting to database
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex:true,
+    useFindAndModify: false 
+}).then(
+        console.log("Database connected"))
+        
+        
+        
+        
+        // relaxing security
+        app.use(cors({
+            origin: 'https://www.section.io',
+            origin: ['https://www.section.io', 'https://www.google.com/'],
+            origin: '*',
+            allowedHeaders: 'X-Requested-With, Content-Type, Authorization',
+            methods: 'GET, POST, PATCH, PUT, POST, DELETE, OPTIONS'
+        }))
+        
+        // parsing req
+        app.use(express.json());
+        
+        // console.log("tanuj https://ridemaker.herokuapp.com/signin ")
+        // calling route to main
+        app.use("", signin)
+        app.use("/settings", settings);
+        app.use("/addvechile", addvechile)
+        app.use("/search", search_ride)
+        
+        
+        
+        app.listen(process.env.PORT, ()=>
+        {
+            console.log(`Server running on port ${process.env.PORT}`)
+        });
+        
+        
+
+
